5628b63fb1da224b17b49016c83d3669abd67303
heinthant <hthant00chk@gmail.com>
2023-03-13 19:44:31 +0630

    Add optional params to classes

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age, name, specialization:)
-    super(age, name)
+  def initialize(age, name = 'Unknown', specialization:, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
